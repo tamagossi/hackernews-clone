6d3e569647506c57d95b6a969afed5f14aff991e
Raka Pratama <mgf.prauliyatama@gmail.com>
2020-02-12 16:52:47 +0700

    feature(redux): connect to index
    connect store into index.js so then the component within the Provider tag can use the tag

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 import './index.css';
 
-import * as serviceWorker from './serviceWorker';
-
 import 'tachyons';
+import store from './redux'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
