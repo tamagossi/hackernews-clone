b4f175c3eb550990e936ca4a904ef42fd53c35f9
Raka Pratama <mgf.prauliyatama@gmail.com>
2020-02-12 15:11:23 +0700

    fix(homepage): remove API class from homepage

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import BodyWrapper from '../../components/body-wrapper';
-import jsonPlaceHolderServices from '../../services/jsonPlaceHolder';
 
 const Homepage = () => {
-  useEffect(() => {
-    jsonPlaceHolderServices.getUser().then((res) => {
-      console.log(res)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }, [])
-
   return (
     <>
       <BodyWrapper>
