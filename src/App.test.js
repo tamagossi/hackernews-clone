54ba89fb08a231c59d632bd796ffe13c0be81d2f
Raka Pratama <mgf.prauliyatama@gmail.com>
2020-02-16 07:15:37 +0700

    feateure(app): test app
    1. Should renders without crashing

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
+import React from 'react';
+
+import { shallow } from 'enzyme';
+import App from './App'
+
+describe(`App component`, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
